e9b8f106fcdd82223db9340a21964cd20cec419e
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-09-20 13:01:57 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById("toggle-theme");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  toggleBtn.textContent = body.classList.contains("dark")
+    ? "☀️ Light Mode"
+    : "🌙 Dark Mode";
+});
